1f17a1069177c9a26d683e09d2247d5f0da60f15
Filip Burlacu <filburli@gmail.com>
2017-10-01 13:34:10 -0400

    Gameplay tweaks: - new downsmash move (jump when hitting ceiling) - better system for preventing spacebar-holding (now, you just have to release between jumps)

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,8 +13,8 @@ const JUMP_SPEED = -900;
 const GRAVITY = 1500;
 const RUN_SPEED = 300;
 
-var speed = 4
-var moving_left = 0
+var speed = 4;
+var moving_left = 0;
 var jump_state = FALLING;
 var velocity = Vector2(0,0);
 var acceleration = Vector2(0,0);
@@ -26,10 +26,13 @@ var anim;
 var raycast;
 var sprite;
 
-var prev_jump_button_state = false;
-var jump_button_state = false;
+var jump_released = false; # flag: set to false after jumping, set to true when jump button is released
+# used to prevent spacebar-holding
 
 var is_pushed_down = false;
+var ceiling_downjump = false;
+
+var smash_anim = false;
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -47,15 +50,16 @@ func switch_to_anim(name, redo=false):
 		anim.play(name);
 
 func kill_player():
+	# disable spike collision
 	set_collision_mask_bit(1, false);
 	switch_to_anim("death");
 	alive = false;
 
 func _fixed_process(delta):
+
+	var jump = Input.is_action_pressed("player_jump");
+	if not jump: jump_released = true; # for ensuring you can't just hold down spacebar
 	
-	prev_jump_button_state = jump_button_state;
-	jump_button_state = Input.is_action_pressed("player_jump");
-	var jump = jump_button_state;# and not prev_jump_button_state;
 	if not alive:
 		var parent = get_parent();
 		if parent == null:
@@ -73,15 +77,15 @@ func _fixed_process(delta):
 		if n.y < -0.01 and is_pushed_down:
 			velocity.x = -velocity.x;
 			is_pushed_down = false;
+			ceiling_downjump = false;
 		if col.get_name() == "spikeTiles":
-			# die
-			# disable layer 2
 			kill_player();
 		elif abs(n.x) - 0.1 > abs(n.y):
 			# steep slope
 			velocity.x = -velocity.x;
 			time_since_grounded = 0;
 			is_pushed_down = false;
+			ceiling_downjump = false;
 		else:
 			# shallow slope 
 			jump_state = LANDED;
@@ -90,28 +94,40 @@ func _fixed_process(delta):
 		if n.y > 0.01:
 			jump_state = FALLING;
 			velocity = n.slide(velocity) + 1.2 * delta * n;
-			is_pushed_down = true;
 			# velocity.y = 1;
+			is_pushed_down = true;
+			ceiling_downjump = true;
+			time_since_grounded = 0;
 		else:
 			is_pushed_down = false;
+			ceiling_downjump = false;
 	elif raycast.is_colliding():
 		is_pushed_down = false;
+		ceiling_downjump = false;
 		jump_state = LANDED;
 		n = raycast.get_collision_normal();
 		if not jump:
 			var p = raycast.get_collision_point();
 			move(p-get_global_pos());
-	elif not jump_button_state:
+	elif not jump:
 		jump_state = FALLING;
 		is_pushed_down = false;
 
-	if jump and (jump_state == LANDED or time_since_grounded < 0.25):
-		velocity.y = JUMP_SPEED;
-		velocity.x *= 1.2;
-		jump_state = JUMPING;
+	if jump and (jump_state == LANDED or time_since_grounded < 0.18) and jump_released:
+		jump_released = false;
 		time_since_grounded = 1000;
-		switch_to_anim("jump", true);
-	if (not jump_button_state or velocity.y > 0) and jump_state == JUMPING:
+		if ceiling_downjump:
+			print("smash");
+			velocity = Vector2(sign(velocity.x) * 0.1, -JUMP_SPEED);
+			switch_to_anim("smash");
+			smash_anim = true;
+		else:
+			velocity.y = JUMP_SPEED;
+			velocity.x *= 1.2;
+			jump_state = JUMPING;
+			switch_to_anim("jump", true);
+		ceiling_downjump = false;
+	if (not jump or velocity.y > 0) and jump_state == JUMPING:
 		jump_state = FALLING;
 	if jump_state == FALLING:
 		if velocity.y < 0: 
@@ -120,6 +136,7 @@ func _fixed_process(delta):
 	# print(JUMP_STATES[jump_state]);
 
 	if jump_state == LANDED:
+		smash_anim = false;
 		acceleration.y = 0;
 		time_since_grounded = 0;
 		velocity = velocity.normalized() * RUN_SPEED;
@@ -136,14 +153,21 @@ func _fixed_process(delta):
 		if alive:
 			if jump_state == JUMPING:
 				switch_to_anim("jump");
+				smash_anim = false;
 			if jump_state == FALLING:
-				switch_to_anim("fall");
+				if smash_anim:
+					switch_to_anim("smash");
+				else:
+					switch_to_anim("fall");
+
 
 	if alive:
 		if velocity.x < 0:
 			sprite.set_scale(Vector2(-1,1));
 		else:
 			sprite.set_scale(Vector2(1,1));
+	else:
+		switch_to_anim("death");
 
 	velocity.y += acceleration.y * delta;
 	if not alive: velocity.x = 0;
